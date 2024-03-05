e9c5f1e32316150363dd69ccd97c4a815f5e67ca
Siddhi118 <sid82256@gmail.com>
2024-03-05 10:55:34 +0530

    Initial commit

---
+++ BaseClass.java
@@ -0,0 +1,85 @@
+package CommonUtils;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseClass {
+//	public static WebDriver d;
+	WebDriver d=new ChromeDriver();
+
+	PropertyFileUtil putil = new PropertyFileUtil();
+	WebDriverUtil wutil = new WebDriverUtil();
+	
+	@BeforeSuite
+	public void BS() {
+		System.out.println("Connect to Data Base");	
+		
+		
+	}
+	
+	@BeforeClass
+	public void BC() throws IOException {
+		//it is used to lanch application
+		String URL=putil.getDataFromPropertyFile("Url");
+		
+		
+		
+//		WebDriver d=new ChromeDriver();
+		wutil.maximize(d);
+		wutil.implicitwait(d);
+			
+		d.get(URL);
+		
+	}
+	
+	@BeforeMethod
+	public void BM() throws IOException {
+		// it is used to login to the application
+		String USERNAME =putil.getDataFromPropertyFile("Username");
+		String PASSWORD=putil.getDataFromPropertyFile("Password");
+		
+		d.findElement(By.name("user_name")).sendKeys(USERNAME);
+		d.findElement(By.name("user_password")).sendKeys(PASSWORD);
+		d.findElement(By.id("submitButton")).click();
+		
+	}
+	
+	@AfterClass
+	public void AC() {
+		// it is used to close the browser
+		d.quit();
+	}
+	
+	@AfterMethod
+	public void AM() throws InterruptedException {
+		// it is used to logout from appliaction
+		Thread.sleep(2000);
+		//moushover on image 
+				WebElement image = d.findElement(By.cssSelector("img[src='themes/softed/images/user.PNG']"));
+				wutil.mousehover(d, image);		
+				
+				//Click on Signout
+				d.findElement(By.xpath("//a[text()='Sign Out']")).click();
+				
+	}
+	
+	@AfterSuite
+	public void AS() {
+		
+		System.out.println("Disconnect from Data Base");
+		
+	}
+	
+	
+
+}
---
+++ ListnerImplimentation.java
@@ -0,0 +1,91 @@
+package CommonUtils;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class ListnerImplimentation implements ITestListener {
+	
+	
+	ExtentReports report;
+	public static void main(String [] args) {
+		
+	}
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		String methodname = result.getMethod().getMethodName();
+		Reporter.log(methodname+" TestScript Execution started" , true);
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		String methodname = result.getMethod().getMethodName();
+		Reporter.log(methodname+" TestScript Execution passed" , true);
+
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		String message = result.getThrowable().toString();
+		String methodname = result.getMethod().getMethodName();
+		Reporter.log(methodname+" TestScript Execution failed "+message , true);
+
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		String methodname = result.getMethod().getMethodName();
+
+		Reporter.log(methodname+" TestScript Execution skipeed" , true);
+
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		
+	}
+
+	@Override
+	public void onTestFailedWithTimeout(ITestResult result) {
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+//		String methodname = result.getMethod().getMethodName();
+//		Reporter.log(" Execution started" , true);
+		
+		// use ExtentSparkReporter class to just configure extentreport
+		JavaUtil jutil = new JavaUtil();
+		ExtentSparkReporter reporter = new ExtentSparkReporter("./extentreport/report"+jutil.getRandomNumber()+".html");
+		reporter.config().setDocumentTitle("Vtigercrm");
+		reporter.config().setTheme(Theme.DARK);
+		reporter.config().setReportName("organization");
+		
+		// use ExtentReports to generate ExtentReport
+		 report = new ExtentReports();
+		report.attachReporter(reporter);
+		report.setSystemInfo("OS", "Window");
+		report.setSystemInfo("Browser", "Chome");
+		report.setSystemInfo("Author", "Sid");
+		
+		
+	}
+
+	
+
+	@Override
+	public void onFinish(ITestContext context) {
+//		String methodname = result.getMethod().getMethodName();
+		Reporter.log(" Execution passed" , true);
+
+		report.flush();
+	}
+
+}
